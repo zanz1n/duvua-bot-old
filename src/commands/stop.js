0e71e856947fbbf25a24d0934b9b7f950cc4054e
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-04-11 23:32:34 -0300

    added stop simple command

---
+++ src/commands/stop.js
@@ -0,0 +1,26 @@
+const { MessageEmbed } = require('discord.js')
+const { QueryType } = require('discord-player')
+const { Permissions } = require('discord.js')
+
+module.exports = {
+    name: "stop",
+    description: "Para o bot e limpa a fila de reprodução",
+    async execute(client, message, args) {
+        const queue = client.player.getQueue(message.guildId)
+
+        let embed = new MessageEmbed()
+        if (!message.member.permissions.has(Permissions.FLAGS.MOVE_MEMBERS)) {
+            return embed.setDescription(`**Você não tem permissão para usar esse comando!,  ${message.author.username}**`),
+                await message.reply({ embeds: [embed] })
+        }
+        if (!queue) {
+            return embed.setDescription(`**Não há nenhum som na fila,  ${message.author.username}**`),
+                await message.reply({ embeds: [embed] })
+        }
+
+        queue.destroy()
+
+        embed.setDescription(`**A fila foi limpa por ${message.author.username}**`)
+        await message.reply({ embeds: [embed] })
+    }
+}
