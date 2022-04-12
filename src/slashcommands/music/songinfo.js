1c84e4ae644a0f4858a2d56ac000cb8bccffaa1d
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-04-11 23:34:58 -0300

    added songInfo slash command

---
+++ src/slashcommands/music/songinfo.js
@@ -0,0 +1,34 @@
+const slashCommand = require('../../slashCommands')
+const { MessageEmbed } = require('discord.js')
+const { Permissions } = require('discord.js')
+
+module.exports = class extends slashCommand {
+    constructor(client) {
+        super(client, {
+            name: "songinfo",
+            description: "Mostra informações do som que está tocando",
+        })
+    }
+
+    async run(interaction) {
+        const queue = this.client.player.getQueue(interaction.guildId)
+
+        let embed = new MessageEmbed()
+
+        if (!queue) {
+            return embed.setDescription(`**Não há nenhum som na fila,  ${interaction.user.username}**`),
+                await interaction.editReply({ embeds: [embed] })
+        }
+
+        let bar = queue.createProgressBar({
+            queue: false,
+            length: 19
+        })
+
+        const song = queue.current
+
+        embed.setThumbnail(song.thumbnail).setDescription(`**${interaction.user.username}**\n\nTocando agora: [${song.title}](${song.url})\n\n**Duração: [${song.duration}]**\n\n ${bar}`)
+
+        await interaction.editReply({ embeds: [embed] })
+    }
+}
