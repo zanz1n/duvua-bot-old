fb04789b1369449025b87b183fe60c3535395e9b
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-04-11 23:34:20 -0300

    added queue slash command

---
+++ src/slashcommands/music/queue.js
@@ -0,0 +1,32 @@
+const slashCommand = require('../../slashCommands')
+const { MessageEmbed } = require('discord.js')
+
+module.exports = class extends slashCommand {
+    constructor(client) {
+        super(client, {
+            name: "queue",
+            description: "Exibe o som que está tocando na fila",
+        })
+    }
+
+    async run(interaction) {
+        const queue = this.client.player.getQueue(interaction.guildId)
+        let embed = new MessageEmbed()
+        if (!queue || !queue.playing) {
+            return embed.setDescription(`**Não há nenhum som na fila!**`),
+                await interaction.editReply({ embeds: [embed] })
+        }
+
+        const queueString = queue.tracks.join("\n")
+        const currentSong = queue.current
+
+        embed.setDescription("**Tocando**\n" +
+            (currentSong ? `\`[${currentSong.duration}]\` ${currentSong.title} -- <@${currentSong.requestedBy.id}>` : None) +
+            `\n\n**Lista**\n${queueString}`
+        )
+            .setFooter({ text: `Requisitado por ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() })
+            .setThumbnail(currentSong.thumbnail)
+
+        interaction.editReply({ embeds: [embed] })
+    }
+}
