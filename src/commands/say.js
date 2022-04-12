9b28f0e69d62daafba047390c06c2f6ef70da6f1
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-04-11 23:31:49 -0300

    added say simple command

--- src/commands/say.js
+++ src/commands/say.js
@@ -1,7 +1,8 @@
 module.exports = {
     name: "say",
     description: "Diz o que é dito para dizer",
-    async execute(message, args) {
-        await message.channel.send(`${args[0]}\n-${message.author}`)
+    async execute(client, message, args) {
+        if (args === undefined) return message.reply(`Você precisa inserir uma mensagem ${message.author}`)
+        await message.channel.send(`${args}\n-${message.author}`)
     }
 }
