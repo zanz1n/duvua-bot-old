1d88cf4cc717256666a47e1aeeaa90cde464e191
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-04-11 23:35:19 -0300

    added say slash command

---
+++ src/slashcommands/utility/say.js
@@ -0,0 +1,21 @@
+const slashCommand = require('../../slashCommands')
+
+module.exports = class extends slashCommand {
+    constructor(client) {
+        super(client, {
+            name: "say",
+            description: "Eu falo o que você me pedir",
+            options: [
+                {
+                    name: "content",
+                    description: "O que você quer que eu fale",
+                    type: 3,
+                    required: true
+                }
+            ]
+        })
+    }
+    async run(interaction) {
+        await interaction.editReply({ content: `${interaction.options.getString('content')}\n-${interaction.user}` })
+    }
+}
