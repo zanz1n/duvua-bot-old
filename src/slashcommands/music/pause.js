dffd9d1bf8558fdb873940b525ba24390422be55
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-04-11 23:33:54 -0300

    added pause slash command

---
+++ src/slashcommands/music/pause.js
@@ -0,0 +1,31 @@
+const slashCommand = require('../../slashCommands')
+const { MessageEmbed } = require('discord.js')
+const { Permissions } = require('discord.js')
+
+module.exports = class extends slashCommand {
+    constructor(client) {
+        super(client, {
+            name: "pause",
+            description: "Pausa a reprodução do bot",
+        })
+    }
+
+    async run(interaction) {
+        const queue = this.client.player.getQueue(interaction.guildId)
+
+        let embed = new MessageEmbed()
+        if (!interaction.member.permissions.has(Permissions.FLAGS.MOVE_MEMBERS)) {
+            return embed.setDescription(`**Você não tem permissão para usar esse comando!,  ${interaction.user.username}**`),
+                await interaction.editReply({ embeds: [embed] })
+        }
+        if (!queue) {
+            return embed.setDescription(`**Não há nenhum som na fila,  ${interaction.user.username}**`),
+                await interaction.editReply({ embeds: [embed] })
+        }
+
+        queue.setPaused(true)
+
+        embed.setDescription(`**Bot pausado por ${interaction.user.username}**\nUse /resume para continuar a reprodução`)
+        await interaction.editReply({ embeds: [embed] })
+    }
+}
