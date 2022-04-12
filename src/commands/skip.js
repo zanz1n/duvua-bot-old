894b33aa227f44610ee8b19757875bf8afa41bc5
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-04-11 23:32:06 -0300

    added skip simple command

---
+++ src/commands/skip.js
@@ -0,0 +1,22 @@
+const { MessageEmbed } = require('discord.js')
+const { QueryType } = require('discord-player')
+const { Permissions } = require('discord.js')
+
+module.exports = {
+    name: "skip",
+    description: "Pula a música que está tocando",
+    async execute(client, message, args) {
+        const queue = client.player.getQueue(message.guildId)
+
+        let embed = new MessageEmbed()
+
+        if (!queue) {
+            return embed.setDescription(`**Não há nenhum som na fila,  ${message.author.username}**`),
+                await message.reply({ embeds: [embed] })
+        }
+        queue.skip()
+
+        embed.setDescription(`**Música** ${queue.current.title} **pulada por ${message.author.username}**`)
+        await message.reply({ embeds: [embed] })
+    }
+}
