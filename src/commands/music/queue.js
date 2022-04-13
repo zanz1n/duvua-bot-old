24a9ac3beb4d1a86361401f2fd6c49e8467a95eb
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-04-13 20:41:51 -0300

    changed queue directory

--- src/commands/music/queue.js
+++ src/commands/music/queue.js
@@ -6,11 +6,12 @@ module.exports = {
     name: "queue",
     description: "Exibe o som que está tocando na fila",
     async execute(client, message, args) {
+        const msg = await message.channel.send("`Pensando [...]`")
         const queue = client.player.getQueue(message.guildId)
         let embed = new MessageEmbed()
         if (!queue || !queue.playing) {
             return embed.setDescription(`**Não há nenhum som na fila!**`),
-                await message.reply({ embeds: [embed] })
+                await msg.edit({ content: " ", embeds: [embed] })
         }
 
         const queueString = queue.tracks.join("\n")
@@ -23,6 +24,6 @@ module.exports = {
             .setFooter({ text: `Requisitado por ${message.author.username}`, iconURL: message.author.displayAvatarURL() })
             .setThumbnail(currentSong.thumbnail)
 
-        message.reply({ embeds: [embed] })
+        msg.edit({ content: " ", embeds: [embed] })
     }
 }
