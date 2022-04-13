70c872d0b9a54d5fd14935787ca4379b2962526c
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-04-13 20:43:22 -0300

    changed onready console log

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,4 +1,5 @@
 const Event = require('../../Event')
+const { memoryUsage } = require('process')
 
 module.exports = class extends Event {
     constructor(client) {
@@ -7,7 +8,8 @@ module.exports = class extends Event {
         })
     }
     run = () => {
-        console.log(`\x1b[34m[bot-api] Client logged to discord-api as ${this.client.user.username} in ${this.client.guilds.cache.size} guild(s)\x1b[0m`)
         this.client.registrySlashCommands()
+        console.log(`\x1b[34m[bot-api] Client logged to discord-api as ${this.client.user.username} in ${this.client.guilds.cache.size} guild(s)\x1b[0m`)
+        //console.log("[bot-api] memoryUsage: " + parseInt(memoryUsage().heapTotal / 1024 ** 2) + "/" + parseInt(memoryUsage().rss / 1024 ** 2) + " MB") //to check memory usage when bot finish load
     }
 }
