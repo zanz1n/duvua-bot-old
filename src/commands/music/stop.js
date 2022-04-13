ed32f006c3f25ec0d8f9f2a3256e1260c7d62b3d
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-04-13 20:42:28 -0300

    changed stop directory

--- src/commands/music/stop.js
+++ src/commands/music/stop.js
@@ -11,16 +11,16 @@ module.exports = {
         let embed = new MessageEmbed()
         if (!message.member.permissions.has(Permissions.FLAGS.MOVE_MEMBERS)) {
             return embed.setDescription(`**Você não tem permissão para usar esse comando!,  ${message.author.username}**`),
-                await message.reply({ embeds: [embed] })
+                await message.channel.send({ embeds: [embed] })
         }
         if (!queue) {
             return embed.setDescription(`**Não há nenhum som na fila,  ${message.author.username}**`),
-                await message.reply({ embeds: [embed] })
+                await message.channel.send({ embeds: [embed] })
         }
 
         queue.destroy()
 
         embed.setDescription(`**A fila foi limpa por ${message.author.username}**`)
-        await message.reply({ embeds: [embed] })
+        await message.channel.send({ embeds: [embed] })
     }
 }
