6164d7a6b66b9e6ee3670e84472d5efbb55e0629
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-04-17 13:57:38 -0300

    member needs a perm to use /embed or /say

--- src/commands/embed.js
+++ src/commands/embed.js
@@ -4,8 +4,13 @@ module.exports = {
     name: "embed",
     description: "Faz uma embed com o que você me disser",
     async execute(client, message, args) {
+        let embed = new MessageEmbed()
+        if (!message.member.permissions.has(Permissions.FLAGS.MANAGE_CHANNELS)) {
+            return embed.setDescription(`**Você não tem permissão para usar esse comando!,  ${interaction.user.username}**`),
+                await message.reply({ embeds: [embed] })
+        }
         if (args === undefined) return message.reply(`Você precisa inserir uma mensagem ${message.author}`)
-        const embed = new MessageEmbed().setTitle(args).setFooter({ text: `Requisitado por ${message.author.username}`, iconURL: message.author.displayAvatarURL() })
+        embed.setTitle(args).setFooter({ text: `Requisitado por ${message.author.username}`, iconURL: message.author.displayAvatarURL() })
         await message.channel.send({ embeds: [embed] }).setTimestamp()
     }
 }
--- src/commands/say.js
+++ src/commands/say.js
@@ -1,8 +1,16 @@
+const { MessageEmbed } = require('discord.js')
+const { Permissions } = require('discord.js')
+
 module.exports = {
     name: "say",
     description: "Diz o que é dito para dizer",
     async execute(client, message, args) {
-        if (args === undefined) return message.reply(`Você precisa inserir uma mensagem ${message.author}`)
+        let embed = new MessageEmbed()
+        if (!message.member.permissions.has(Permissions.FLAGS.MANAGE_CHANNELS)) {
+            return embed.setDescription(`**Você não tem permissão para usar esse comando!,  ${interaction.user.username}**`),
+                await message.reply({ embeds: [embed] })
+        }
+        if (args === undefined || args.length <= 1) return message.reply(`Você precisa inserir uma mensagem ${message.author}`)
         await message.channel.send(`${args}\n-${message.author}`)
     }
 }
