98759866e50538746f73976b8fc53c9224b1a09c
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-04-16 22:54:49 -0300

    user needs manage_channels perm to use /say

--- src/slashcommands/utility/say.js
+++ src/slashcommands/utility/say.js
@@ -1,4 +1,5 @@
 const slashCommand = require('../../slashCommands')
+const { Permissions } = require('discord.js')
 
 module.exports = class extends slashCommand {
     constructor(client) {
@@ -16,6 +17,10 @@ module.exports = class extends slashCommand {
         })
     }
     async run(interaction) {
+        if (!interaction.member.permissions.has(Permissions.FLAGS.MANAGE_CHANNELS)) {
+            return embed.setDescription(`**Você não tem permissão para usar esse comando!,  ${interaction.user.username}**`),
+                await interaction.editReply({ embeds: [embed] })
+        }
         await interaction.editReply({ content: `${interaction.options.getString('content')}\n-${interaction.user}` })
     }
 }
