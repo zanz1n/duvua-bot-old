4a7fe32b046bb13f26038f655f7246efa20e93f8
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-04-16 22:39:39 -0300

    corrected a bug in avatar command

--- src/commands/avatar.js
+++ src/commands/avatar.js
@@ -6,14 +6,14 @@ module.exports = {
     async execute(client, message, args) {
         const [...messageargs] = args.slice(client.prefix.length).split(/ +/)
         const user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author
-        const member = message.mentions.members.first() || client.members.cache.get(args[0]) || message.member
+        const member = message.mentions.members.first() || message.member
 
         const embed = new MessageEmbed().setColor(member.displayHexColor)
         const image = user.displayAvatarURL({ dynamic: true, size: 2048 })
 
         embed.setAuthor("Avatar de " + user.username, user.displayAvatarURL()).setImage(image)
             .setFooter({ text: `Requisitado por ${message.author.username}`, iconURL: message.author.displayAvatarURL() })
-            .setTimestamp().setDescription(`[__**Ver Original**__](${user.displayAvatarURL({ format: 'png' })})`).setTimestamp()
+            .setTimestamp().setDescription(`**Clique [aqui](${user.displayAvatarURL({ format: 'png' })}) para ver original!**`).setTimestamp()
         await message.channel.send({ embeds: [embed] })
     }
 }
