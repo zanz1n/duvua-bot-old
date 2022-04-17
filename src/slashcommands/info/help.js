84ff028965e69b64f75f5ede4d56eacaeb8239a7
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-04-16 22:53:07 -0300

    using the command description in /help

--- src/slashcommands/info/help.js
+++ src/slashcommands/info/help.js
@@ -10,31 +10,35 @@ module.exports = class extends slashCommand {
     }
     async run(interaction) {
         let embed = new MessageEmbed()
+        const find = (command) => {
+            return this.client.slashCommands.find(c => c.name === command).description
+        }
         embed.setTitle("Help")
-            .setDescription(`**${this.client.user} pode fazer muitas coisas para o seu servidor.**\nO que inclui tocar músicas, ver avatar de usuários, beijar alguém e até fazer um meme, confira todos os comandos do bot abaixo:`)
+            .setDescription(`**${this.client.user} pode fazer muitas coisas para o seu servidor.**\nO que inclui tocar músicas, ver avatar de usuários, beijar alguém e até fazer um meme, confira todos os comandos do bot abaixo:\n\n**\:globe_with_meridians: - slash commands | \:m: - legacy commands**\n\n\:globe_with_meridians: - digite:  **/**  para ver as opções; \:m: - podem ser usados com o prefixo:  **${this.client.prefix}**  no chat`)
 
             .addField("Música", "Todos os comandos relacionados a músicas.")
 
-            .addField("play", "Toca uma música do youtube.", true).addField("queue", "Exibe o som que está tocando na fila.", true)
-            .addField("pause", "Pausa a reprodução do bot.", true).addField("resume", "Despausa a reprodução do bot.", true)
-            .addField("skip", "Pula a música que está tocando.", true).addField("songinfo", "Mostra o som que está tocando.", true)
-            .addField("stop", "Para o bot e limpa a fila de reprodução", true).addField("*", "*", true).addField("*", "*", true)
+            .addField("\:globe_with_meridians:\:m: play", find("play"), true).addField("\:globe_with_meridians:\:m: queue", find("queue"), true) //proposital error
+            .addField("\:globe_with_meridians: pause", find("pause"), true).addField("\:globe_with_meridians: resume", find("resume"), true)
+            .addField("\:globe_with_meridians:\:m: skip", find("skip"), true).addField("\:globe_with_meridians:\:m: songinfo", find("songinfo"), true)
+            .addField("\:globe_with_meridians:\:m: stop", find("stop"), true).addField("*", "*", true).addField("*", "*", true)
 
             .addField("Info / Utilidade", "Comandos de info ou de utilidade.")
 
-            .addField("ping", "Responde com pong e mostra o meu ping.", true).addField("info", "Exibe informações sobre o bot.", true)
-            .addField("embed", "Faço uma embed com o que me disser.", true).addField("say", "Eu falo o que você me pedir.", true)
-            .addField("help", "Exibe as funções e os comandos do bot.", true).addField("*", "*", true)
+            .addField("\:globe_with_meridians:\:m: ping", find("ping"), true).addField("\:globe_with_meridians: info", find("info"), true)
+            .addField("\:globe_with_meridians:\:m: embed", find("embed"), true).addField("\:globe_with_meridians:\:m: say", find("say"), true)
+            .addField("\:globe_with_meridians: help", find("help"), true).addField("*", "*", true)
 
             .addField("Fun", "Comandos for fun e de diversão em geral.")
 
-            .addField("kiss", "Beija alguém.", true).addField("avatar", "Exibe o avatar de alguém.", true)
+            .addField("\:globe_with_meridians: kiss", find("kiss"), true).addField("\:globe_with_meridians:\:m: avatar", find("avatar"), true)
             .addField("*", "*", true)
 
             .addField("Tem alguma função legal que gostaria de ver no bot ou alguma sugestão?", "Sugira no nosso [servidor do discord](https://discord.com), a dm <@586600481959182357>, ou para coisas mais técnicas o [nosso github](https://github.com).")
             .setThumbnail(this.client.user.displayAvatarURL())
 
             .setFooter({ text: `Requisitado por ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() }).setTimestamp()
-        await interaction.reply({ embeds: [embed], ephemeral: true })
+
+        await interaction.editReply({ embeds: [embed], ephemeral: true })
     }
 }
