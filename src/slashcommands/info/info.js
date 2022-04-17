686f899e3f85b9cb96cc2ecbf981f4a3f3021f1d
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-04-16 22:53:30 -0300

    some changes in /info

--- src/slashcommands/info/info.js
+++ src/slashcommands/info/info.js
@@ -10,10 +10,11 @@ module.exports = class extends slashCommand {
     }
     async run(interaction) {
         let embed = new MessageEmbed()
-        embed.setTitle("Sobre Mim").addField("Feita por", "<@586600481959182357>, alguém mais?\n")
+        embed.setTitle("Sobre Mim").setDescription("Feita por <@586600481959182357>\n")
             .addField("Ajude o bot a continuar online!", "Para que o bot continue online, precisamos desenbolsar um custo mensal de ${cost}, ajude o projeto a continuar clicando **[aqui](https://www.youtube.com/).**\n")
             .setThumbnail(this.client.user.displayAvatarURL())
             .addField("Comandos básicos:", `**help** - mostra os comandos do bot e dicas, use /help ou ${this.client.prefix}help\n**ping** - mostra o ping do bot, use /ping ou ${this.client.prefix}ping\n\n*Para ver todos os comandos do bot, use /commands ou ${this.client.prefix}commands *\n`)
+            .addField("Tem alguma função legal que gostaria de ver no bot ou alguma sugestão?", "Sugira no nosso [servidor do discord](https://discord.com), a dm <@586600481959182357>, ou para coisas mais técnicas o [nosso github](https://github.com).")
         await interaction.editReply({ embeds: [embed] })
     }
 }
