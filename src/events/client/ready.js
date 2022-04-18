d75bec3300fd24d2986aaa580867bc37322e5bd3
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-04-17 23:59:14 -0300

    changed the color of the logs

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -11,7 +11,7 @@ module.exports = class extends Event {
         this.client.user.setActivity(`type /help or ${this.client.prefix}help`);
 
         this.client.registrySlashCommands()
-        console.log(`\x1b[34m[bot-api] Client logged to discord-api as ${this.client.user.username} in ${this.client.guilds.cache.size} guild(s)\x1b[0m`)
-        console.log("\x1b[34m[bot-api] memoryUsage: " + parseInt(memoryUsage().heapTotal / 1024 ** 2) + "/" + parseInt(memoryUsage().rss / 1024 ** 2) + " MB\x1b[0m") //to check memory usage when bot finish load
+        console.log(`\x1b[33m[bot-api] Client logged to discord-api as ${this.client.user.username} in ${this.client.guilds.cache.size} guild(s)\x1b[0m`)
+        console.log("\x1b[33m[bot-api] memoryUsage: " + parseInt(memoryUsage().heapTotal / 1024 ** 2) + "/" + parseInt(memoryUsage().rss / 1024 ** 2) + " MB\x1b[0m") //to check memory usage when bot finish load
     }
 }
