15c343520d1f4d794bc1522f4cd76e757db56dea
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-04-17 23:59:45 -0300

    @mention the bot now is the same as help command

--- src/events/client/messageCreate.js
+++ src/events/client/messageCreate.js
@@ -7,13 +7,22 @@ module.exports = class extends Event {
         })
     }
     run = async (message) => {
-        if (message.author.bot || !message.content.startsWith(this.client.prefix)) return
-        
-        const [msgcommand] = await message.content.trim().substring(this.client.prefix.length).split(/\s+/)//.toLowerCase()
+        const client = this.client
+        if (message.author.bot) return
 
-        const cmd = this.client.commands.find(el => el.name === msgcommand)
-        const args = message.content.replace(this.client.prefix + msgcommand, "")
+        if (message.content.startsWith(this.client.prefix)) {
 
-        cmd.execute(this.client, message, args)
+            const [msgcommand] = await message.content.trim().substring(this.client.prefix.length).split(/\s+/)
+            const args = message.content.replace(this.client.prefix + msgcommand, "")
+
+            const cmd = this.client.commands.find(el => el.name === msgcommand.toLowerCase())
+            if (!cmd) return
+
+            cmd.execute(this.client, message, args)
+        } else {
+            const [msgcommand] = await message.content.trim().substring(client.prefix.length).split(/\s+/)
+            const args = message.content.replace(this.client.prefix + msgcommand, "")
+            if (message.content === `<@${this.client.user.id}>`) return require("../../commands/info/help").execute(this.client, message, args)
+        }
     }
 }
