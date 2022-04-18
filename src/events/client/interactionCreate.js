5e2ab14a7925e0e3270742c579a7a65177458805
zanz1n <izanrodrigues999@gmail.com>
2022-04-18 11:37:10 +0000

    prevent bugs in interactionCreate event

--- src/events/client/interactionCreate.js
+++ src/events/client/interactionCreate.js
@@ -10,25 +10,16 @@ module.exports = class extends Event {
 
         if (!interaction.isCommand || interaction.user.bot) return
         const cmd = await this.client.slashCommands.find(c => c.name === interaction.commandName)
+
+        if(!cmd) return
+
         const ephemeral = (string) => interaction.commandName === string //boolean
 
         if (ephemeral("help") || ephemeral("info")) {
             await interaction.deferReply({ ephemeral: true })
-
         } else {
             await interaction.deferReply()
         }
         cmd.run(interaction)
     }
 }
-/*
- *catch (err) {
-            if (interaction.user.id === "586600481959182357") return interaction.editReply({ content: "```diff\n-Erro: algo de errado aconteceu durante a execução desse comando!\n```" + `\`\n${err}\`` })
-            interaction.editReply({ content: "```diff\n-Erro: algo de errado aconteceu durante a execução desse comando!\n```" })
-        }
- *
- * 
- * 
- * 
- * 
- */
