796c841c9be7a198ed90726f56b674e37c6ca5e5
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-04-22 18:16:43 -0300

    removed commented lines

--- src/events/client/messageCreate.js
+++ src/events/client/messageCreate.js
@@ -23,3 +23,21 @@ module.exports = class extends Event {
         else if (message.content === `<@${this.client.user.id}>`) return require("../../commands/info/help").execute(this.client, message, args)
     }
 }
+/*
+ *const user = await this.client.database.user.findOne({ _id: message.author.id })
+        const guild = await this.client.database.guild.findOne({ _id: message.guild.id })
+        const Client = await this.client.database.user.findOne({ _id: this.client.user.id })
+
+        if (!user) await this.client.database.user.create({ _id: message.author.id, name: message.author.tag })
+        if (!guild) await this.client.database.guild.create({ _id: message.guild.id, name: message.guild.name })
+        if (!Client) await this.client.database.client.create({ _id: this.client.user.id })
+
+        if (user.name !== message.author.tag) await this.client.database.user.findOneAndUpdate({ _id: message.author.id }, { $set: { name: message.author.tag } })
+        if (guild.name !== message.guild.name) await this.client.database.guild.findOneAndUpdate({ _id: message.guild.id }, { $set: { name: message.guild.name } })
+ *
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
