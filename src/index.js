cd5627b4c9ad0d542305b80ba9ccf743a6880121
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-04-22 18:14:44 -0300

    moved the client class to /structures

--- src/index.js
+++ src/index.js
@@ -1,86 +1,15 @@
 console.log(`\x1b[33m[bot-api] Starting [...]\x1b[0m`)
 
-const Discord = require('discord.js')
-const { Client, MessageEmbed, Intents } = require('discord.js')
-const fs = require('fs')
-const { join } = require('path')
-const { Player } = require('discord-player')
-const { Permissions } = require('discord.js')
+const { Intents } = require('discord.js')
+const Bot = require('./structures/Client')
+//const mongoose = require('./database/')
 
-require('dotenv').config()
+const config = require('./config.json')
 
-const prefix = "-"
-class ClienT extends Client {
-    constructor(options) {
-        super(options)
-        this.prefix = prefix
-        this.commands = []
-        this.slashCommands = []
-        this.loadCommands()
-        this.loadSlashCommands()
-        this.loadEvents()
-        this.loadPlayer()
-    }
-    loadSlashCommands(path = 'src/slashcommands') {
-        const categories = fs.readdirSync(path)
-        for (const category of categories) {
-            const slashCommands = fs.readdirSync(`${path}/${category}`)
-
-            for (const slashCommand of slashCommands) {
-                const slashCommandClass = require(join(process.cwd(), `${path}/${category}/${slashCommand}`))
-                const scmd = new (slashCommandClass)(this)
-
-                this.slashCommands.push(scmd)
-                console.log(`\x1b[35m[bot-slashCommands] ${scmd.name} loaded\x1b[0m`)
-            }
-        }
-        console.log(`\x1b[33m[bot-api] all slashCommands loaded\x1b[0m`)
-    }
-    loadCommands(path = 'src/commands') {
-        const categories = fs.readdirSync(path)
-        for (const category of categories) {
-            const commands = fs.readdirSync(`${path}/${category}`)
-
-            for (const command of commands) {
-                const cmd = require(join(process.cwd(), `${path}/${category}/${command}`))
-
-                this.commands.push(cmd)
-                console.log(`\x1b[34m[bot-legacyCommands] ${cmd.name} loaded\x1b[0m`)
-            }
-        }
-        console.log(`\x1b[33m[bot-api] All legacyCommand loaded\x1b[0m`)
-    }
-    loadEvents(path = 'src/events') {
-        const categories = fs.readdirSync(path)
-        for (const category of categories) {
-            const events = fs.readdirSync(`${path}/${category}`)
-
-            for (const event of events) {
-                const eventClass = require(join(process.cwd(), `${path}/${category}/${event}`))
-                const evt = new (eventClass)(this)
-
-                this.on(evt.name, evt.run)
-                console.log(`\x1b[36m[bot-events] Event ${evt.name} loaded\x1b[0m`)
-            }
-        }
-    }
-    registrySlashCommands() {
-        this.guilds.cache.get(process.env.GUILD_ID).commands.set(this.slashCommands) //for Dev
-        // this.application.commands.set(this.commands) //for Production environment
-    }
-    loadPlayer() {
-        this.player = new Player(this, {
-            ytdlOptions: {
-                quality: "highestaudio",
-                highWaterMark: 1 << 25
-            }
-        })
-    }
-}
-
-const client = new ClienT({
+const client = new Bot({
     intents: [
         Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MEMBERS,
         Intents.FLAGS.GUILD_MESSAGES,
         Intents.FLAGS.GUILD_VOICE_STATES,
         Intents.FLAGS.GUILD_MESSAGE_REACTIONS
@@ -88,6 +17,4 @@ const client = new ClienT({
     partials: ['MESSAGE', 'REACTION']
 })
 
-module.exports = ClienT
-
-client.login(process.env.TOKEN)
+client.login(config.token)
