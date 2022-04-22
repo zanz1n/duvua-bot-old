bf8b349c5f2f92dbb2bb3602fecd56e5bb31a14c
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-04-22 18:19:09 -0300

    changed the dont have perm message in /say

--- src/slashcommands/utility/say.js
+++ src/slashcommands/utility/say.js
@@ -18,8 +18,8 @@ module.exports = class extends slashCommand {
     }
     async run(interaction) {
         if (!interaction.member.permissions.has(Permissions.FLAGS.MANAGE_CHANNELS)) {
-            return embed.setDescription(`**Você não tem permissão para usar esse comando!,  ${interaction.user.username}**`),
-                await interaction.editReply({ embeds: [embed] })
+            embed.setDescription(`**Você não tem permissão para usar esse comando,  ${interaction.user.username}**`)
+            return await interaction.editReply({ embeds: [embed] })
         }
         await interaction.editReply({ content: `${interaction.options.getString('content')}\n-${interaction.user}` })
     }
