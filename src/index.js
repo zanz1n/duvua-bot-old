39fd79537172e400d7af8c3e9bf3ee7ffe0e9331
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-04-23 04:21:32 -0300

    removed unused intents

--- src/index.js
+++ src/index.js
@@ -11,8 +11,7 @@ const client = new Bot({
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MEMBERS,
         Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_VOICE_STATES,
-        Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+        Intents.FLAGS.GUILD_VOICE_STATES
     ],
     partials: ['MESSAGE', 'REACTION']
 })
