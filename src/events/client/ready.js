5a4018416c48149902ebea80ae64503ff862d55b
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-04-22 23:07:50 -0300

    changed bot activity

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -9,7 +9,7 @@ module.exports = class extends Event {
     run = async () => {
         await this.client.connectToDatabase()
 
-        this.client.user.setActivity(`type /help or ${this.client.prefix}help`);
+        this.client.user.setActivity(`use /help`);
 
         this.client.registrySlashCommands()
         console.log(`\x1b[33m[bot-api] logged to discord-api as ${this.client.user.tag} in ${this.client.guilds.cache.size} guild(s)\x1b[0m`)
