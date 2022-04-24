1d82c0d9093db2c943cec09a18b33acc34d38ffd
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-04-24 02:08:21 -0300

    removed commented lines

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -15,20 +15,4 @@ module.exports = class extends Event {
         console.log(`\x1b[33m[bot-api] logged to discord-api as ${this.client.user.tag} in ${this.client.guilds.cache.size} guild(s)\x1b[0m`)
     }
 }
-/*
- *  this.client.database = {
-        user: User,
-        command: Command,
-        guild: Guild,
-        client: Client
-    }
- *
- *  const mongoose = require('mongoose')
- * 
- *  const Client = require('../../database/models/client')
-    const User = require('../../database/models/user')
-    const Guild = require('../../database/models/guild')
-    const Command = require('../../database/models/command')
- * 
- * 
- */
+
