c2f7e147336f02d20678b364c93c3ed52033c027
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-04-25 01:24:19 -0300

    just a few change in info command

--- src/slashcommands/info/info.js
+++ src/slashcommands/info/info.js
@@ -13,8 +13,8 @@ module.exports = class extends slashCommand {
         embed.setTitle("Sobre Mim").setDescription("Feita por <@586600481959182357>\n")
             .addField("Ajude o bot a continuar online!", "Para que o bot continue online, precisamos desenbolsar um custo mensal de ${cost}, ajude o projeto a continuar clicando **[aqui](https://www.youtube.com/).**\n")
             .setThumbnail(this.client.user.displayAvatarURL())
-            .addField("Comandos básicos:", `**help** - mostra os comandos do bot e dicas, use /help ou ${this.client.prefix}help\n**ping** - mostra o ping do bot, use /ping ou ${this.client.prefix}ping\n\n*Para ver todos os comandos do bot, use /commands ou ${this.client.prefix}commands *\n`)
+            .addField("Comandos básicos:", `**help** - mostra os comandos do bot e dicas, use /help ou ${this.client.prefix}help\n**ping** - mostra o ping do bot, use /ping ou ${this.client.prefix}ping\n\n`)
             .addField("Tem alguma função legal que gostaria de ver no bot ou alguma sugestão?", "Sugira no nosso [servidor do discord](https://discord.com), a dm <@586600481959182357>, ou para coisas mais técnicas o [nosso github](https://github.com).")
-        await interaction.editReply({ embeds: [embed] })
+        await interaction.editReply({ content: null, embeds: [embed] })
     }
 }
