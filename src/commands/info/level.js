737b84b6e99371944d908715dc3b1f0eb5a2329b
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-04-25 15:01:17 -0300

    now you can specify the member to see the level

--- src/commands/info/level.js
+++ src/commands/info/level.js
@@ -4,10 +4,29 @@ module.exports = {
     name: "level",
     description: "Mostra o seu level de interação no servidor",
     async execute(client, message, args) {
-        const meta = 3 * (message.member.db.level ** 2)
-        const embed = new MessageEmbed().setDescription(`**Seu level atual no servidor ${message.guild.name} é ${message.member.db.level}**
-        Seu progresso para atingir o level ${message.member.db.level + 1} é ${message.member.db.xp}/${meta} pontos!`
-        )
+        const embed = new MessageEmbed()
+
+        const user = await message.mentions.users.first() || message.author
+        if (user.bot) {
+            embed.setDescription(`**${user} é um bot, ${message.author.username}**`)
+            return message.reply({ content: null, embeds: [embed] })
+        }
+
+        const mensioned = await client.db.member.findById(message.guild.id + user.id) ||
+            new client.db.member({ _id: message.guild.id + user.id, guildid: message.guild.id, userid: user.id, usertag: user.tag });
+
+        mensioned.save()
+
+        const meta = 3 * (mensioned.level ** 2)
+        if (user.id === message.author.id) {
+            embed.setDescription(`**Seu level atual no servidor ${message.guild.name} é ${mensioned.level}**
+            Para que você atinja o level ${mensioned.level + 1} serão necessários mais ${meta - mensioned.xp} pontos! (${mensioned.xp}/${meta})`
+            )
+        } else {
+            embed.setDescription(`**O level atual de ${user.username} em ${message.guild.name} é ${mensioned.level}**
+            Para que ${user.username} atinja o level ${mensioned.level + 1} serão necessários mais ${meta - mensioned.xp} pontos! (${mensioned.xp}/${meta})`
+            )
+        }
 
         message.reply({ content: null, embeds: [embed] })
     }
--- src/slashcommands/info/level.js
+++ src/slashcommands/info/level.js
@@ -7,7 +7,15 @@ module.exports = class extends slashCommand {
     constructor(client) {
         super(client, {
             name: "level",
-            description: "Mostra o seu level de interação no servidor"
+            description: "Mostra o de interação no servidor de alguém",
+            options: [
+                {
+                    name: "pessoa",
+                    description: "De quem você deseja ver o level",
+                    type: 6,
+                    required: false
+                }
+            ]
         })
     }
     /**
@@ -17,10 +25,30 @@ module.exports = class extends slashCommand {
      * 
      */
     async run(interaction) {
-        const meta = 3 * (interaction.member.db.level ** 2)
-        const embed = new MessageEmbed().setDescription(`**Seu level atual no servidor ${interaction.guild.name} é ${interaction.member.db.level}**
-        Seu progresso para atingir o level ${interaction.member.db.level + 1} é ${interaction.member.db.xp}/${meta} pontos!`
-        )
+        const embed = new MessageEmbed()
+        const options = interaction.options._hoistedOptions
+        const user = (options.find((e) => e.name === "pessoa") && options.find((e) => e.name === "pessoa").member.user) || interaction.user
+
+        if (user.bot) {
+            embed.setDescription(`**${user} é um bot, ${interaction.user.username}**`)
+            return interaction.editReply({ content: null, embeds: [embed] })
+        }
+
+        const mensioned = await this.client.db.member.findById(interaction.guild.id + user.id) ||
+            new this.client.db.member({ _id: interaction.guild.id + user.id, guildid: interaction.guild.id, userid: user.id, usertag: user.tag });
+
+        mensioned.save()
+
+        const meta = 3 * (mensioned.level ** 2)
+        if (user.id === interaction.user.id) {
+            embed.setDescription(`**Seu level atual no servidor ${interaction.guild.name} é ${mensioned.level}**
+            Para que você atinja o level ${mensioned.level + 1} serão necessários mais ${meta - mensioned.xp} pontos! (${mensioned.xp}/${meta})`
+            )
+        } else {
+            embed.setDescription(`**O level atual de ${user.username} em ${interaction.guild.name} é ${mensioned.level}**
+            Para que ${user.username} atinja o level ${mensioned.level + 1} serão necessários mais ${meta - mensioned.xp} pontos! (${mensioned.xp}/${meta})`
+            )
+        }
 
         interaction.editReply({ content: null, embeds: [embed] })
     }
