6b9aba32b75a848a15b180484a8723646c542dc5
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-04-26 20:35:58 -0300

    now the bruno command is better

--- src/commands/fun/bruno.js
+++ src/commands/fun/bruno.js
@@ -1,7 +1,15 @@
+const { MessageEmbed } = require('discord.js')
+
 module.exports = {
     name: "bruno",
     description: "Não falamos do bruno",
     async execute(client, message, args) {
-        await message.channel.send(`Não falamos do bruno ${message.author}`)
+        await message.reply({
+            content: null,
+            embeds: [new MessageEmbed()
+                .setDescription(`**Não falamos do Bruno${message.author}\nMas clique [aqui](https://youtu.be/dWcrZv5p7Jg) para ver essa obra de arte**`)
+                .setThumbnail("https://i.ytimg.com/vi/dWcrZv5p7Jg/maxresdefault.jpg")
+            ]
+        })
     }
 }
