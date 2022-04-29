a0ef17d67bfa69a0152e8a76a393d5efe75b83d3
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-04-29 00:27:19 -0300

    changed help command

--- src/slashcommands/info/help.js
+++ src/slashcommands/info/help.js
@@ -21,9 +21,13 @@ module.exports = class extends slashCommand {
             return this.client.slashCommands.find(c => c.name === command).description
         }
         embed.setTitle("Help")
-            .setDescription(`**${this.client.user} pode fazer muitas coisas para o seu servidor.**\nO que inclui tocar músicas, ver avatar de usuários, beijar alguém e até fazer um meme, confira todos os comandos do bot abaixo:\n\n**\:globe_with_meridians: - slash commands | \:m: - legacy commands**\n\n\:globe_with_meridians: - digite:  **/**  para ver as opções; \:m: - podem ser usados com o prefixo:  **${this.client.prefix}**  no chat`)
+            .setDescription(`
+            **${this.client.user} pode fazer muitas coisas para o seu servidor.**
+            O que inclui tocar músicas, ver avatar de usuários, beijar alguém e até fazer um meme, confira todos os comandos do bot abaixo:\n
+            **\:globe_with_meridians: - slash commands | \:m: - legacy commands**\n
+            \:globe_with_meridians: - digite:  **/**  para ver as opções; \:m: - podem ser usados com o prefixo:  **${interaction.guild.db.prefix}** no chat`)
 
-            .addField("Categorias", "```diff\n+Para visualizar todas as categorias e comandos use a caixa de seleção abaixo.\n```")
+            .addField("ℹ️ Categorias", "**Para visualizar todas as categorias e comandos use a caixa de seleção abaixo.**")
             //imutable
             //end of imutable
             .setThumbnail(this.client.user.displayAvatarURL())
@@ -33,11 +37,11 @@ module.exports = class extends slashCommand {
         const row = new MessageActionRow().addComponents(
             new MessageSelectMenu().setCustomId('1').setPlaceholder('Escolha a categoria').setMinValues(1).setMaxValues(1)
                 .addOptions([
-                    { label: "Config", value: "config", description: "Comandos para configurar o bot no servidor", emoji: "⚙️" },
                     { label: "Fun", value: "fun", description: "Comandos para descontrair", emoji: "🥳" },
                     { label: "Info", value: "info", description: "Comandos para ver informações sobre o bot", emoji: "ℹ️" },
                     { label: "Moderation / Utility", value: "mod-util", description: "Comandos para auxiliar na moderação e organização do server", emoji: "🖋️" },
                     { label: "Music", value: "music", description: "Comandos para tocar musicas do youtube", emoji: "🎧" },
+                    { label: "Money / Level", value: "money", description: "Comandos relacionados ao sistema monetário e de ranks do bot", emoji: "💸" },
                 ])
         )
 
@@ -47,17 +51,8 @@ module.exports = class extends slashCommand {
 
         collector.on("collect", async (i) => {
             const values = i.values[0]
-            if (values === "config") {
-                const configEmbed = new MessageEmbed().addField("Config", "Comandos para configurar o bot no servidor.")
-                    .addField("\:globe_with_meridians: config wellcome", "Altera a mensagem de boas vindas", true)
-                    .addField("\:globe_with_meridians: config prefix", "Altera o prefixo do bot", true)
-                    .addField("*", "*", true)
-
-                i.deferUpdate()
-                interaction.editReply({ content: null, embeds: [configEmbed] })
-            }
-            else if (values === "fun") {
-                const funEmbed = new MessageEmbed().addField("Fun", "Comandos for fun e de diversão em geral.")
+            if (values === "fun") {
+                const funEmbed = new MessageEmbed().addField("🥳 Fun", "Comandos for fun e de diversão em geral.")
                     .addField("\:globe_with_meridians: kiss", find("kiss"), true).addField("\:globe_with_meridians:\:m: avatar", find("avatar"), true)
                     .addField("\:m: bruno", "Não falamos do Bruno", true)
 
@@ -65,34 +60,46 @@ module.exports = class extends slashCommand {
                 interaction.editReply({ content: null, embeds: [funEmbed] })
             }
             else if (values === "info") {
-                const infoEmbed = new MessageEmbed().addField("Info", "Comandos de inforomação em geral.")
+                const infoEmbed = new MessageEmbed().addField("ℹ️ Info", "Comandos de inforomação em geral.")
                     .addField("\:globe_with_meridians:\:m: ping", find("ping"), true).addField("\:globe_with_meridians: info", find("info"), true)
                     .addField("\:globe_with_meridians:\:m: help", find("help"), true).addField("\:globe_with_meridians:\:m: anime", find("anime"), true)
-                    .addField("\:globe_with_meridians:\:m: level", find("level"), true).addField("\:globe_with_meridians:\:m: coins", find("coins"), true)
+                    .addField("\:globe_with_meridians: github", find("github"), true).addField("*", "*", true)
 
                 i.deferUpdate()
                 interaction.editReply({ content: null, embeds: [infoEmbed] })
             }
             else if (values === "mod-util") {
-                const configEmbed = new MessageEmbed().addField("Utilidade / Moderação", "Comandos para auxiliar na moderação e organização do server.")
+                const modUtilEmbed = new MessageEmbed().addField("🖋️ Utilidade / Moderação", "Comandos para auxiliar na moderação e organização do server.")
                     .addField("\:globe_with_meridians:\:m: embed", find("embed"), true).addField("\:globe_with_meridians:\:m: say", find("say"), true)
-                    .addField("*", "*", true)
+                    .addField("\:globe_with_meridians: clear", find("clear"), true).addField("\:globe_with_meridians: ban", find("ban"), true)
+                    .addField("\:globe_with_meridians: config wellcome", "Altera a mensagem de boas vindas", true)
+                    .addField("\:globe_with_meridians: config prefix", "Altera o prefixo do bot no servidor", true)
+
+                    .addField("\:information_source: Dica", "**Nos comandos say e embed, use /n para quebrar linha.**")
 
                 i.deferUpdate()
-                interaction.editReply({ content: null, embeds: [configEmbed] })
+                interaction.editReply({ content: null, embeds: [modUtilEmbed] })
             }
             else if (values === "music") {
-                const configEmbed = new MessageEmbed().addField("Música", "Todos os comandos relacionados a músicas.")
+                const musicEmbed = new MessageEmbed().addField("🎧 Música", "Todos os comandos relacionados a músicas.")
                     .addField("\:globe_with_meridians:\:m: play", find("play"), true).addField("\:globe_with_meridians:\:m: queue", find("queue"), true)
                     .addField("\:globe_with_meridians: pause", find("pause"), true).addField("\:globe_with_meridians: resume", find("resume"), true)
                     .addField("\:globe_with_meridians:\:m: skip", find("skip"), true).addField("\:globe_with_meridians:\:m: songinfo", find("songinfo"), true)
                     .addField("\:globe_with_meridians:\:m: stop", find("stop"), true).addField("*", "*", true).addField("*", "*", true)
 
                 i.deferUpdate()
-                interaction.editReply({ content: null, embeds: [configEmbed] })
+                interaction.editReply({ content: null, embeds: [musicEmbed] })
+            }
+            else if (values === "money") {
+                const moneyEmbed = new MessageEmbed().addField("💸 Dinheiro", "Todos os comandos relacionados ao sistema monetário.")
+                    .addField("\:globe_with_meridians:\:m: coins", find("coins"), true).addField("\:globe_with_meridians: \:m: work", find("work"), true)
+                    .addField("\:globe_with_meridians:\:m: level", find("level"), true)
+
+                i.deferUpdate()
+                interaction.editReply({ content: null, embeds: [moneyEmbed] })
             }
 
         })
         await interaction.editReply({ embeds: [embed], components: [row] })
     }
-}
+} //.addField("*", "*", true)
