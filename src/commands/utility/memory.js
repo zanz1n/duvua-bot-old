c12b99c6a6daebd7958dee55c20a0f170ef5efbd
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-04-28 23:02:54 -0300

    changed not permited message in memory command

--- src/commands/utility/memory.js
+++ src/commands/utility/memory.js
@@ -7,9 +7,11 @@ module.exports = {
     async execute(client, message, args) {
         const embed = new MessageEmbed()
         if (message.author.id != "586600481959182357") {
-            embed.setDescription(`**Desculpe, mas apenas meu dono pode usar esse comando,  ${message.author.username}**`)
+            embed.setDescription(`**Desculpe, você não pode usar esse comando,  ${message.author.username}**`)
             return await message.reply({ embeds: [embed] })
         }
-        await message.channel.send("logs:\n```diff\n-[bot-api] memoryUsage: " + `${parseInt(memoryUsage().heapTotal / 1024 ** 2)}(USED)` + " / " + `${parseInt(memoryUsage().rss / 1024 ** 2)}(ALOCATED)` + " MB\n```")
+        await message.channel.send("logs:\n```diff\n-[bot-api] memoryUsage: " +
+            `${parseInt(memoryUsage().heapTotal / 1024 ** 2)}(USED)` +
+            " / " + `${parseInt(memoryUsage().rss / 1024 ** 2)}(ALOCATED)` + " MB\n```")
     }
 }
